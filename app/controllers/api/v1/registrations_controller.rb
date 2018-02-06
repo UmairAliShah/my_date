508b9717de8af23aa4ba209bb4bcfbccbd946ab4
UmairAliShah <umairali43230@gmail.com>
2018-02-06 16:07:10 +0500

    Commit

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::RegistrationsController < ApplicationController
   skip_before_action :authenticate_user_from_token!, :only => [:create], :raise => false
 
   def create
+    #debugger
     begin
       if params[:password] == params[:password_confirmation]
         @user = User.new(:email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
